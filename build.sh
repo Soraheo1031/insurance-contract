c483dcc9e10d6be783c6874c7a85bca6584c0b3c
Sora Heo <srheo1031@gmail.com>
2021-07-05 17:28:36 +0900

    yaml 추가

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@ build()
 {
     cd $1
     mvn package -B -D maven.test.skip=true
-    #docker build -t soraHeo/$1:$2 .
-    #docker push soraHeo/$1:$2
-    cd ..    
+    #docker build -t goang486/$1:$2 .
+    #docker push goang486/$1:$2
+    docker build -t 095661863019.dkr.ecr.ap-northeast-2.amazonaws.com/$1:$2 .
+    docker push 095661863019.dkr.ecr.ap-northeast-2.amazonaws.com/$1:$2
+    cd ..
 }
 
 ver=v1
-if [ $# -gt 0 ] 
+if [ $# -gt 0 ]
 then
     ver=$1
 fi
