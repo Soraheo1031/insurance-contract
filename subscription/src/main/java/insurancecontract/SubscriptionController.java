5bafdf6eee0092be690b26faf16d378cd386bf5b
Sora Heo <srheo1031@gmail.com>
2021-07-08 13:28:00 +0900

    failure

--- subscription/src/main/java/insurancecontract/SubscriptionController.java
+++ subscription/src/main/java/insurancecontract/SubscriptionController.java
@@ -1,6 +1,7 @@
 package insurancecontract;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
