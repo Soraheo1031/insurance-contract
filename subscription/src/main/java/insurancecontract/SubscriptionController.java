e053d0d171ca1b90a715044677a0b5a8dfd21e62
Sora Heo <srheo1031@gmail.com>
2021-07-08 13:43:40 +0900

    live

--- subscription/src/main/java/insurancecontract/SubscriptionController.java
+++ subscription/src/main/java/insurancecontract/SubscriptionController.java
@@ -1,7 +1,6 @@
 package insurancecontract;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,28 +11,5 @@ import java.util.List;
 
  @RestController
  public class SubscriptionController {
-    @GetMapping("/callmemleak")
-    public void callMemLeak() {
-    try {
-        this.memLeak();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-    }
 
-    public void memLeak() throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
-        Class unsafeClass = Class.forName("sun.misc.Unsafe");
-        
-        Field f = unsafeClass.getDeclaredField("theUnsafe");
-        f.setAccessible(true);
-        Unsafe unsafe = (Unsafe) f.get(null);
-        System.out.print("4..3..2..1...");
-        try {
-            for(;;)
-            unsafe.allocateMemory(1024*1024);
-        } catch(Error e) {
-            System.out.println("Boom!");
-            e.printStackTrace();
-        }
-    }
  }
