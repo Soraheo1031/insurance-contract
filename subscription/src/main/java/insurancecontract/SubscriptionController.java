9a6fbe0f4b24c55d8ce0ab0a4e44a1c79c838713
Sora Heo <srheo1031@gmail.com>
2021-07-08 12:17:58 +0900

    bomb

--- subscription/src/main/java/insurancecontract/SubscriptionController.java
+++ subscription/src/main/java/insurancecontract/SubscriptionController.java
@@ -11,6 +11,28 @@ import java.util.List;
 
  @RestController
  public class SubscriptionController {
+    @GetMapping("/callmemleak")
+    public void callMemLeak() {
+    try {
+        this.memLeak();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    }
 
-
+    public void memLeak() throws NoSuchFieldException, ClassNotFoundException, IllegalAccessException {
+        Class unsafeClass = Class.forName("sun.misc.Unsafe");
+        
+        Field f = unsafeClass.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        Unsafe unsafe = (Unsafe) f.get(null);
+        System.out.print("4..3..2..1...");
+        try {
+            for(;;)
+            unsafe.allocateMemory(1024*1024);
+        } catch(Error e) {
+            System.out.println("Boom!");
+            e.printStackTrace();
+        }
+    }
  }
